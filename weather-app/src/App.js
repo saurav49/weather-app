2e88a446297e1777089fbf9db98b5b9e4e0bf4ef
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2022-05-29 00:07:48 +0530

    chore
    - navbar created

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,3 +1,9 @@
+import { Navbar } from "./component/index";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
---
+++ weather-app/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="uppercase bg-amber-400 text-white tracking-wide text-md font-normal py-2 px-5 w-full text-right md:text-center ">
+      my weather
+    </nav>
+  );
+};
+
+export { Navbar };
